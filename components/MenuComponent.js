a1606042ad947d523630384b3503ac5a7663f53e
gondolio <oren.sivan@gmail.com>
2019-03-16 10:07:42 +0900

    Components Part 2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, FlatList } from 'react-native';
+import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 
 function Menu(props) {
@@ -11,6 +11,7 @@ function Menu(props) {
                     title={item.name}
                     subtitle={item.description}
                     hideChevron={true}
+                    onPress={() => props.onPress(item.id)}
                     leftAvatar={{ source: require('./images/uthappizza.png')}}
                     />
             );
