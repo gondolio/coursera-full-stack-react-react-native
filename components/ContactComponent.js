432d9c5a7626aab3136844d24484b5520c25bb74
gondolio <oren.sivan@gmail.com>
2019-04-16 09:44:37 +0900

    ESlint fix ContactComponent

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
@@ -5,28 +6,28 @@ import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
 
-    static navigationOptions = {
-        title: 'Contact Us'
-    };
+  static navigationOptions = {
+    title: 'Contact Us',
+  };
 
-    render() {
-        return(
-            <ScrollView>
-                <Animatable.View animation="fadeInDown" duration={2000}>
-                    <Card title='Contact Information'>
-                        <Text style={{margin: 10, lineHeight: 50, fontSize: 18}}>
-{`121, Clear Water Bay Road
+  render() {
+    return (
+      <ScrollView>
+        <Animatable.View animation="fadeInDown" duration={2000}>
+          <Card title="Contact Information">
+            <Text style={{ margin: 10, lineHeight: 50, fontSize: 18 }}>
+              {`121, Clear Water Bay Road
 Clear Water Bay, Kowloon
 HONG KONG
 Tel: +852 1234 5678
 Fax: +852 8765 4321
 Email:confusion@food.net`}
-                        </Text>
-                    </Card>
-                </Animatable.View>                
-            </ScrollView>
-        );
-    }
+            </Text>
+          </Card>
+        </Animatable.View>
+      </ScrollView>
+    );
+  }
 }
 
 export default Contact;
